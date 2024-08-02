3d36177ad6691374c767b21a89f4b58dc9f04c1e
Mixail132 <millionbaxov@gmail.com>
2024-08-03 00:13:32 +0300

    Add a new file to glue logic.

--- app/glue.py
+++ app/glue.py
@@ -1,12 +1,7 @@
 import subprocess
 
 
-def glue_scripts(
-        script_1,
-        script_2,
-        script_3,
-        output_script
-):
+def glue_scripts(script_1, script_2, script_3, output_script, file):
 
     with (open(script_1, "r") as s1,
           open(script_2, "r") as s2,
@@ -34,14 +29,16 @@ def glue_scripts(
 if __name__ == "__main__":
     file_1 = "vars.py"
     file_2 = "telegram.py"
-    file_3 = "main.py"
+    file_3 = "viber.py"
+    file_4 = "main.py"
     output_file = "common.py"
 
     glue_scripts(
         file_1,
         file_2,
         file_3,
-        output_file
+        file_4,
+        output_file,
     )
 
     subprocess.run(
