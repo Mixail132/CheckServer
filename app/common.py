4f83206ed7dd6fa4b0cc6229e51c822a28300bcc
Mixail132 <millionbaxov@gmail.com>
2024-08-03 00:38:49 +0300

    Glue the files.

--- app/common.py
+++ app/common.py
@@ -3,6 +3,9 @@ import subprocess
 import time
 
 import telebot
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages.text_message import TextMessage
 
 
 class IniSection(configparser.ConfigParser):
@@ -21,6 +24,11 @@ class Plant:
         self.telegram_users = {
             user: tg_id for user,
             tg_id in parser.section["TELEGRAM_USERS"].items()}
+        self.viber_users = {user: tg_id for user,
+                            tg_id in parser.section["VIBER_USERS"].items()}
+        self.viber_configs = {
+            par_name: par_value for par_name,
+            par_value in parser.section["VIBER_CONFIGS"].items()}
         self.tokens = {
             social_media: token for social_media,
             token in parser.section["TOKENS"].items()}
@@ -38,9 +46,28 @@ telegramtoken = plants.tokens["telegramtoken"]
 bot = telebot.TeleBot(telegramtoken)
 
 
-def send_alarm_message(message_text):
+def send_telegram_message(message_text):
     for user in plants.telegram_users.values():
         bot.send_message(user, message_text)
+
+
+vibertoken = plants.tokens["vibertoken"]
+viberavatar = plants.viber_configs["BOT_AVATAR"]
+vibername = plants.viber_configs["BOT_NAME"]
+
+bot_config = BotConfiguration(
+    name=vibername,
+    avatar=viberavatar,
+    auth_token=vibertoken
+)
+
+viber = Api(bot_config)
+
+
+def send_viber_message(message_text):
+    message = TextMessage(text=message_text)
+    for user_id in plants.viber_users.values():
+        viber.send_messages(user_id, [message])
 
 
 def is_server_out(ip_addr):
@@ -76,7 +103,9 @@ def ping_servers(vent_units):
             continue
 
         elif all(servers_out) and not vent_units.sendings[shield]:
-            send_alarm_message(f"{vent_units.messages[shield]}")
+            alarm_message_text = vent_units.messages[shield]
+            send_telegram_message(alarm_message_text)
+            send_viber_message(alarm_message_text)
             vent_units.sendings[shield] = True
 
         elif not all(servers_out):
