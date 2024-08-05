ae3647b417b4fc8b4e238ab64e63050a23eb2323
Mixail132 <millionbaxov@gmail.com>
2024-08-05 11:23:16 +0300

    Proccess the error if the Viber recipient is unsubscribed.

--- app/common.py
+++ app/common.py
@@ -70,10 +70,22 @@ bot_config = BotConfiguration(
 viber = Api(bot_config)
 
 
-def send_viber_message(message_text):
-    message = TextMessage(text=message_text)
+def send_viber_message(alarm_message):
+    alarm_msg = TextMessage(text=alarm_message)
     for user_id in plants.viber_users.values():
-        viber.send_messages(user_id, [message])
+        try:
+            viber.send_messages(user_id, [alarm_msg])
+        except Exception as ex:
+            if "notSubscribed" in ex.args[0]:
+                recipients = {
+                    name: chat_id for chat_id,
+                    name in plants.viber_users.items()}
+                bot_admin = plants.viber_users["admin"]
+                byby_message = f"{recipients[user_id]} has left this chat."
+                byby_msg = TextMessage(text=byby_message)
+                viber.send_messages(bot_admin, [byby_msg])
+                del recipients, bot_admin
+                continue
 
 
 def is_server_out(ip_addr):
