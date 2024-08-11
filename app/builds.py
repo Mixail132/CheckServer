45701b4a53f408d9f22306550078fe6d778992ae
Mixail132 <millionbaxov@gmail.com>
2024-08-11 20:33:02 +0300

    Remove the second instance of the app and rename its files.

---
+++ app/builds.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import subprocess
+
+DIR_ROOT = Path(__file__).parent.resolve()
+DIR_APP = DIR_ROOT / "app"
+
+def glue_scripts(script_1, script_2, script_3, script_4,  output_script):
+
+    with (open(script_1, "r") as s1,
+          open(script_2, "r") as s2,
+          open(script_3, "r") as s3,
+          open(script_4, "r") as s4,
+          open(output_script, "w") as out):
+
+        for script in [s1, s2, s3, s4]:
+            for line in script:
+                if "app." in line:
+                    continue
+                elif "__name__" in line:
+                    break
+                out.write(line)
+            out.write("\n\n")
+
+
+if __name__ == "__main__":
+    file_1 = DIR_APP / "vars.py"
+    file_2 = DIR_APP / "telegram.py"
+    file_3 = DIR_APP / "viber.py"
+    file_4 = DIR_APP / "main.py"
+    output_file = DIR_APP / "commons.py"
+
+    glue_scripts(
+        file_1,
+        file_2,
+        file_3,
+        file_4,
+        output_file,
+    )
+
+    subprocess.run(
+        [
+         "autopep8",
+         "--in-place",
+         "--aggressive",
+         output_file
+        ]
+    )
+
+    subprocess.run(
+        [
+         "isort",
+         output_file
+        ]
+    )
--- app/viber.py
+++ app/viber.py
@@ -1,11 +1,11 @@
 from viberbot import Api
-from app.vars import plants
+from app.vars import allvars
 from viberbot.api.bot_configuration import BotConfiguration
 from viberbot.api.messages.text_message import TextMessage
 
-vibertoken = plants.tokens["VIBERTOKEN"]
-viberavatar = plants.viber_configs["BOT_AVATAR"]
-vibername = plants.viber_configs["BOT_NAME"]
+vibertoken = allvars.viber_configs["VIBERTOKEN"]
+viberavatar = allvars.viber_configs["BOT_AVATAR"]
+vibername = allvars.viber_configs["BOT_NAME"]
 
 bot_config = BotConfiguration(
     name=vibername,
@@ -18,15 +18,21 @@ viber = Api(bot_config)
 
 def send_viber_message(alarm_message):
     alarm_msg = TextMessage(text=alarm_message)
-    for user_id in plants.viber_users.values():
+    for user_id in allvars.viber_users.values():
         try:
             viber.send_messages(user_id, [alarm_msg])
         except Exception as ex:
             if "notSubscribed" in ex.args[0]:
-                recipients = {name: chat_id for chat_id, name in plants.viber_users.items()}
-                bot_admin = plants.viber_users["admin"]
+                recipients = {name: chat_id for chat_id, name in allvars.viber_users.items()}
+                bot_admin = allvars.viber_users["admin"]
                 byby_message = f"{recipients[user_id]} has left this chat."
                 byby_msg = TextMessage(text=byby_message)
                 viber.send_messages(bot_admin, [byby_msg])
                 del recipients, bot_admin
                 continue
+
+
+if __name__ == "__main__":
+    admin = allvars.viber_users["admin"]
+    check_message = TextMessage(text="Check the bot!")
+    viber.send_messages(admin, check_message)
