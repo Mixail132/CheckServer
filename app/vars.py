c0d14fde79cdb7b771d938c9548213f2c0de2e95
Mixail132 <millionbaxov@gmail.com>
2024-08-12 14:09:09 +0300

    Fix linter R0903 note (Too few public methods).

--- app/vars.py
+++ app/vars.py
@@ -1,6 +1,8 @@
 """ Picking the variables from 'ini' configure file. """
 import configparser
 from pathlib import Path
+from dataclasses import dataclass
+from typing import Any
 
 DIR_ROOT = Path(__file__).parent.parent.resolve()
 DIR_APP = DIR_ROOT / "app"
@@ -16,43 +18,46 @@ class IniSection(configparser.ConfigParser):
         return self._sections
 
 
+@dataclass
 class Vars:
     """ Keeps all the configuration variables. """
-
-    def __init__(self) -> None:
-        """ Reads the config file and matches its variables. """
-
-        configs = IniSection()
-        configs.read("vars.ini", "utf-8")
-        parser = configs["VARS"].parser
-        headers = parser.sections()
-
-        sources = []
-        for net_type in ["WIFI", "DLAN", "INET"]:
-            sources += [source for source in headers if net_type in source]
-
-        self.hosts = {
-            source: parser.section[f"{source}"] for source in sources
-        }
-        self.telegram_users = dict(parser.section["TELEGRAM_USERS"].items())
-
-        self.viber_users = dict(parser.section["VIBER_USERS"].items())
-
-        self.viber_configs = {
-            par.upper(): value for par, value in parser.section["VIBER_CONFIGS"].items()
-        }
-        self.telegram_configs = {
-            par.upper(): value for par, value in parser.section["TELEGRAM_CONFIGS"].items()
-        }
-        self.messages = {
-            source: parser.section["MESSAGES"][f"{source.lower()}"] for source in self.hosts
-        }
-        self.sendings = {
-            source: False for source in self.hosts
-        }
+    viber_configs: dict[Any, Any]
+    viber_users: dict[Any, Any]
+    viber_configs: dict[Any, Any]
+    telegram_users: dict[Any, Any]
+    telegram_configs: dict[Any, Any]
+    hosts: dict[Any, Any]
+    sendings: dict[Any, Any]
+    messages: dict[Any, Any]
 
 
-allvars = Vars()
+configs = IniSection()
+configs.read("vars.ini", "utf-8")
+parser = configs["VARS"].parser
+headers = parser.sections()
+sources = []
+for net_type in ["WIFI", "DLAN", "INET"]:
+    sources += [source for source in headers if net_type in source]
+
+
+hosts = {source: parser.section[f"{source}"] for source in sources}
+telegram_users = dict(parser.section["TELEGRAM_USERS"].items())
+viber_users = dict(parser.section["VIBER_USERS"].items())
+telegram_configs = {par.upper(): value for par, value in parser.section["TELEGRAM_CONFIGS"].items()}
+viber_configs = {par.upper(): value for par, value in parser.section["VIBER_CONFIGS"].items()}
+messages = {source: parser.section["MESSAGES"][f"{source.lower()}"] for source in hosts}
+sendings = {source: False for source in hosts}
+
+
+allvars = Vars(
+    hosts=hosts,
+    telegram_users=telegram_users,
+    viber_users=viber_users,
+    telegram_configs=telegram_configs,
+    viber_configs=viber_configs,
+    messages=messages,
+    sendings=sendings
+)
 
 
 if __name__ == "__main__":
