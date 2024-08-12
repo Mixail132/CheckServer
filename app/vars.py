abfba90508eee68c997e7381975b2462af715d9a
Mixail132 <millionbaxov@gmail.com>
2024-08-12 10:48:25 +0300

    Fix linters' notes.

--- app/vars.py
+++ app/vars.py
@@ -15,6 +15,7 @@ class IniSection(configparser.ConfigParser):
 
 class Vars:
     def __init__(self) -> None:
+
         configs = IniSection()
         configs.read("vars.ini", "utf-8")
         parser = configs["VARS"].parser
@@ -23,16 +24,28 @@ class Vars:
         sources = []
         for net_type in ["WIFI", "DLAN", "INET"]:
             sources += [source for source in headers if net_type in source]
-        self.hosts = {source: parser.section[f"{source}"] for source in sources}
 
-        self.telegram_users = {user: tid for user, tid in parser.section["TELEGRAM_USERS"].items()}
-        self.viber_users = {user: vid for user, vid in parser.section["VIBER_USERS"].items()}
-
-        self.viber_configs = {par.upper(): value for par, value in parser.section["VIBER_CONFIGS"].items()}
-        self.telegram_configs = {par.upper(): value for par, value in parser.section["TELEGRAM_CONFIGS"].items()}
-
-        self.messages = {source: parser.section["MESSAGES"][f"{source.lower()}"] for source in self.hosts}
-        self.sendings = {source: False for source in self.hosts}
+        self.hosts = {
+            source: parser.section[f"{source}"] for source in sources
+        }
+        self.telegram_users = {
+            user: tid for user, tid in parser.section["TELEGRAM_USERS"].items()
+        }
+        self.viber_users = {
+            user: vid for user, vid in parser.section["VIBER_USERS"].items()
+        }
+        self.viber_configs = {
+            par.upper(): value for par, value in parser.section["VIBER_CONFIGS"].items()
+        }
+        self.telegram_configs = {
+            par.upper(): value for par, value in parser.section["TELEGRAM_CONFIGS"].items()
+        }
+        self.messages = {
+            source: parser.section["MESSAGES"][f"{source.lower()}"] for source in self.hosts
+        }
+        self.sendings = {
+            source: False for source in self.hosts
+        }
 
 
 allvars = Vars()
