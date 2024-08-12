4537ad8f09d8b34487be0f7c11afd1afff80dc0c
Mixail132 <millionbaxov@gmail.com>
2024-08-12 17:34:46 +0300

    Process the connection timeout error.

--- app/telegram.py
+++ app/telegram.py
@@ -2,6 +2,7 @@
 
 import telebot
 
+from requests.exceptions import ConnectTimeout
 from app.vars import allvars
 
 telegramtoken = allvars.telegram_configs["TELEGRAMTOKEN"]
@@ -11,7 +12,10 @@ bot = telebot.TeleBot(telegramtoken)
 def send_telegram_message(message_text: str) -> None:
     """ Sends a message to an existing Telegram bot. """
     for user in allvars.telegram_users.values():
-        bot.send_message(user, message_text)
+        try:
+            bot.send_message(user, message_text)
+        except ConnectTimeout:
+            continue
 
 
 if __name__ == "__main__":
