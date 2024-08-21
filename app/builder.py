11ac377acec396297d7578cc3b95776d05d2705c
Mixail132 <millionbaxov@gmail.com>
2024-08-21 19:25:01 +0300

    Fix linters' notes.

--- app/builder.py
+++ app/builder.py
@@ -1,8 +1,8 @@
 """ Actions for building the output 'exe' file. """
 
-from pathlib import Path
-from app.vars import DIR_APP, DIR_STATIC, DIR_TEMP
 import subprocess
+from pathlib import Path
+from app.vars import DIR_APP, DIR_TEMP, DIR_STATIC
 
 
 def glue_scripts(
