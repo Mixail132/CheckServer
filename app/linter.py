1ccb7bb1c759dee494f1a4569c8408d4bafa8a48
Mixail132 <millionbaxov@gmail.com>
2024-08-25 19:06:44 +0300

    Check 'tests' folder when launching the linters.

--- app/linter.py
+++ app/linter.py
@@ -2,18 +2,42 @@
 
 import subprocess
 
-from app.dirs import DIR_APP, DIR_LINTERS
+from app.dirs import DIR_APP, DIR_LINTERS, DIR_TESTS
 
 
 def run_linters():
     """Launches the linters with their settings."""
 
     commands = (
-        ["pylint", f"--rcfile={DIR_LINTERS / '.pylintrc'}", "app"],
-        ["isort", "-c", DIR_APP],
-        ["flake8", "--config", f"{DIR_LINTERS / '.flake8'}", DIR_APP],
-        ["black", "--diff", "--config", f"{DIR_LINTERS / '.black'}", "."],
-        ["mypy", "--config-file", f"{DIR_LINTERS / 'mypy.ini'}", DIR_APP],
+        [
+            "pylint",
+            f"--rcfile={DIR_LINTERS / '.pylintrc'}",
+            DIR_APP,
+            DIR_TESTS,
+        ],
+        ["isort", "-c", DIR_APP, DIR_TESTS],
+        [
+            "flake8",
+            "--config",
+            f"{DIR_LINTERS / '.flake8'}",
+            DIR_APP,
+            DIR_TESTS,
+        ],
+        [
+            "black",
+            "--diff",
+            "--config",
+            f"{DIR_LINTERS / '.black'}",
+            DIR_APP,
+            DIR_TESTS,
+        ],
+        [
+            "mypy",
+            "--config-file",
+            f"{DIR_LINTERS / 'mypy.ini'}",
+            DIR_APP,
+            DIR_TESTS,
+        ],
     )
     for command in commands:
         print(
