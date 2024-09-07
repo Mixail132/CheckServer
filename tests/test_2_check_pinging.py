606faeae2290a843958bfad6baa1b5907a9c91c5
Mixail132 <millionbaxov@gmail.com>
2024-09-07 11:19:53 +0300

    Define an 'always in touch' host as 'www.google.com'

--- tests/test_2_check_pinging.py
+++ tests/test_2_check_pinging.py
@@ -14,9 +14,8 @@ def test_base_ping_settings_work_fine(config_vars_set: Vars) -> None:
     """Checks the project's ping job using always in touch server."""
 
     vars_ = config_vars_set
-    inet_hosts = vars_.hosts["INET SOURCE"]
     audit_shields = AuditShields(vars_)
-    in_touch_host = inet_hosts["IN_TOUCH"]
+    in_touch_host = "www.google.com"
     is_host_out = audit_shields.ping_host(in_touch_host)
 
     assert is_host_out is False
